68e59e983aa1c0dc6f284da4d11721bd998bff7a
Shrey Baxi <baxishrey@gmail.com>
2018-11-26 02:21:24 +0530

    Scroll with jump

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { names } from './names';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import {map, startWith, tap} from 'rxjs/operators';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'virtual-scroll-demo';
+export class AppComponent implements OnInit {
+  names = names;
+  sortedNames = names.slice().sort();
+  nameSearchTextBox = new FormControl('');
+
+  filteredOptions: Observable<string[]>;
+
+  @ViewChild(CdkVirtualScrollViewport) viewPort: CdkVirtualScrollViewport;
+
+  searchButtonDisabled = true;
+
+  ngOnInit() {
+    this.filteredOptions = this.nameSearchTextBox.valueChanges
+      .pipe(
+        startWith(''),
+        tap(value => {if (value.length < 1) {this.searchButtonDisabled = true; }}),
+        map(value => value.length >= 1 ? this._filter(value) : [])
+      );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.names.filter(name => name.toLowerCase().startsWith(filterValue));
+  }
+
+  optionSelected() {
+    this.searchButtonDisabled = false;
+  }
+
+  goToSelectedName() {
+    const selectedName = this.nameSearchTextBox.value;
+    const index = this.sortedNames.indexOf(selectedName);
+    this.viewPort.scrollToIndex(index, 'smooth');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { SortPipe } from './sort.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SortPipe
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ScrollingModule,
+    AngularFontAwesomeModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
